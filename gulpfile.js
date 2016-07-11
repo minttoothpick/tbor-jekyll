7625f7354385ac3852d8f7f8faa0bae02b58a061
Jeremy Jones <jeroum@gmail.com>
2016-07-11 09:25:31 -0400

    Add Susy source path

--- gulpfile.js
+++ gulpfile.js
@@ -50,7 +50,7 @@ gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
 gulp.task('sass', function() {
   return gulp.src('_scss/main.scss')
     .pipe(sass({
-      includePaths: ['scss'],
+      includePaths: ['node_modules/susy/sass', 'scss'],
       onError: browserSync.notify
     }))
     .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
